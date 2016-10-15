a5b5300ffb1c92cd3991faff2ea6fb5f8c824fe3
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2016-10-15 16:42:28 -0300

    Agrego y configuro SonataAdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,13 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            //Dependencias
+			new Sonata\CoreBundle\SonataCoreBundle(),
+			new Sonata\BlockBundle\SonataBlockBundle(),
+			new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+			new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+			//Sonata Bundles
+			new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
